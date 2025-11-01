42ff05178b7d8a8e4023f609eb871ed25cb22bac
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-11-02 00:29:58 +0530

    feat: enhance API route and UI for article summarization with error handling and async support

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,26 +1,91 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 import { Mistral } from "@mistralai/mistralai";
 import { load } from "cheerio";
-import pdf from "pdf-parse"; 
+const pdf = require('pdf-parse');
 
 const apiKey = process.env.MISTRAL_API_KEY
 const client = new Mistral({ apiKey: apiKey })
 
-async function getTextFromURL(url: string){
-    const data = fetch(url)
-    return data
-}
-
-async function dataFromPDF(file: File) {
-    
-}
-
-export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const { url }  = body
-    if(!url){
-        console.log("No url is provided in the input field")
+async function getTextFromURL(url: string) {
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(`Failed to fetch url: ${response.statusText}`)
+        }
+        const html = response.text()
+        const $ = load(await html)
+        return response.json()
+    } catch (error) {
+        console.log("Error scraping url: ", error)
+        throw new Error('Could not scrap from the provided url.')
     }
-    let textToSummarize = getTextFromURL(url)
-    console.log(textToSummarize)
+}
+
+async function getTextFromPDF(file: File) {
+    try {
+        //convert the file to a buffer for pdf-parse
+        const arrayBuffer = await file.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+
+        //parse the pdf
+        const data = await pdf(file)
+        return data.text
+    } catch (error) {
+        console.log("Error parsing pdf: ", error)
+        throw new Error('Could not parse data from the provided pdf')
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+
+        const contentType = request.headers.get('content-type') || ''
+        let textToSummarize: string;
+
+        // get text from request
+        if (contentType.includes('application/json')) {
+            // it is a url
+            const body = await request.json();
+            const { url } = body;
+            if (!url) {
+                console.log("No url is provided in the input field")
+            }
+            textToSummarize = await getTextFromURL(url);
+        } else if (contentType.includes('multipart/form-data')) {
+            // it is a PDF file
+            const formData = await request.formData();
+            const file = formData.get('file') as File;
+            if (!file) {
+                throw new Error('No file provided in form data')
+            }
+            textToSummarize = await getTextFromPDF(file)
+        } else {
+            throw new Error(`Unsupported Content-Type: ${contentType}`)
+        }
+
+        if (!textToSummarize) {
+            throw new Error('Could not extract any text to summarize.')
+        }
+
+        const prompt = `
+            Please provide a concise, expert summary of the following text.
+            Focus on the key points, main arguments, and conclusions.
+            Format the output as clean markdown.
+
+            TEXT: 
+            """
+            ${textToSummarize}
+            """
+        `
+
+        const chatResponse = await client.chat.complete({
+            model: 'mistral-small-latest',
+            messages: [{ role: 'user', content: prompt }]
+        })
+
+        // success message
+        return NextResponse.json({ chatResponse }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: "Invalid request" }, { status: 400 })
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,41 +4,51 @@ import { GoDownload } from "react-icons/go";
 
 export default function Home() {
 
-	const handleMagic = (e: { preventDefault: () => void; target: { value: any; }[]; }) => {
-		e.preventDefault();
+    const handleMagic = async (e: { preventDefault: () => void; target: { value: any; }[]; }) => {
+        e.preventDefault();
         let article = e.target[0].value
-        
-        // fetch('https://localhost:8080/api/')
-	}
 
-	return (
-		<>
-			<div className="w-full h-screen flex justify-center items-center flex-col">
-				<div className="p-2">
-					<h1 className="text-4xl text-center tracking-tight font-bold">Article Summariser</h1>
-					<p className="text-slate-800">paste any article link and get an instant AI-powered summary</p>
-				</div>
-				<div className="border-4 p-4 rounded-md">
-					<div>
-						<form method="post" className="space-x-1 space-y-2" onSubmit={handleMagic}>
-							<input type="url" name="" id="" className="border-2 rounded-lg px-2 py-1 w-100" placeholder="Enter the url" />
-							<button className="hover:bg-black hover:text-white border-2 rounded-lg px-3 py-1 cursor-pointer" type="submit" id="submit-magic">Magic</button>
-						</form>
-					</div>
-					<div className="w-full h-[350px] border-2 rounded-sm">
-						<div className="flex justify-around mt-2 space-x-3">
-							<div className="flex justify-center items-center p-1 space-x-1">
-								<CgNotes />
-								<h2>Summary</h2>
-							</div>
-							<div className="flex justify-center items-center cursor-pointer space-x-1 border p-2 rounded-md hover:bg-black hover:text-white">
-								<GoDownload className="cursor-pointer" />
-								<button type="submit" id="export" className="cursor-pointer">Export</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+        const data = await fetch('http://localhost:3000/api/summarize', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ url: article })
+        })
+
+        const summarizedText = await data.json()
+
+        console.log(summarizedText)
+    }
+
+    return (
+        <>
+            <div className="w-full h-screen flex justify-center items-center flex-col">
+                <div className="p-2">
+                    <h1 className="text-4xl text-center tracking-tight font-bold">Article Summariser</h1>
+                    <p className="text-slate-800">paste any article link and get an instant AI-powered summary</p>
+                </div>
+                <div className="border-4 p-4 rounded-md">
+                    <div>
+                        <form method="post" className="space-x-1 space-y-2" onSubmit={handleMagic}>
+                            <input type="url" name="" id="" className="border-2 rounded-lg px-2 py-1 w-100" placeholder="Enter the url" />
+                            <button className="hover:bg-black hover:text-white border-2 rounded-lg px-3 py-1 cursor-pointer" type="submit" id="submit-magic">Magic</button>
+                        </form>
+                    </div>
+                    <div className="w-full h-[350px] border-2 rounded-sm">
+                        <div className="flex justify-around mt-2 space-x-3">
+                            <div className="flex justify-center items-center p-1 space-x-1">
+                                <CgNotes />
+                                <h2>Summary</h2>
+                            </div>
+                            <div className="flex justify-center items-center cursor-pointer space-x-1 border p-2 rounded-md hover:bg-black hover:text-white">
+                                <GoDownload className="cursor-pointer" />
+                                <button type="submit" id="export" className="cursor-pointer">Export</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
