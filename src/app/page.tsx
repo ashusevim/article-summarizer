b263084f78026a6f7a7c3a59116b72444e358a2d
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-11-06 20:33:53 +0530

    fix: enhance error handling by including response message in HTTP error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,8 @@ export default function Home() {
             })
 
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`)
+                const errorText = await response.text();
+                throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`)
             }
 
             const result = await response.json()
